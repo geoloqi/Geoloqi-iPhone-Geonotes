a568577b90db558c0b7f35e044ca0155f2281a41
Aaron Parecki <aaron@parecki.com>
2012-07-07 15:41:01 -0700

    Remove keys

--- Geonotes/Geonotes/LQConfig.h
+++ Geonotes/Geonotes/LQConfig.h
@@ -1,2 +1,2 @@
-static NSString *const LQ_APIKey = @"44a1f66f7bf41d9063f1fe5c558f40dc";
-static NSString *const LQ_APISecret = @"a7dc66e533847611c8f109db57ce50d6";
+static NSString *const LQ_APIKey = @"";
+static NSString *const LQ_APISecret = @"";
