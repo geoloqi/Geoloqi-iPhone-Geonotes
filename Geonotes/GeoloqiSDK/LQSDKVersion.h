b754a297ee30e727f4cc86384819280b37fad632
Aaron Parecki <aaron@parecki.com>
2012-07-07 12:46:46 -0700

    Adds Geoloqi SDK files

---
+++ Geonotes/GeoloqiSDK/LQSDKVersion.h
@@ -0,0 +1,13 @@
+//
+//  LQSDKVersion.h
+//  GeoloqiSDK
+//
+//  Copyright (c) 2012 Geoloqi, Inc. All rights reserved.
+//
+
+static NSString *const LQSDKNameString = @"com.geoloqi.iphone";
+
+static NSString *const LQSDKVersionString = @"12.160";
+
+// This build string is automatically updated when the sdk is built! Do not modify.
+static NSString *const LQSDKVersionGitVersion = @"79b2524207766577f2e84c2ca93a8366b22d9011";
