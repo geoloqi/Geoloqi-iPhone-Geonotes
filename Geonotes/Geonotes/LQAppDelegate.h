25cc7be37bc7635e4d847b10059bfda15d5e33ab
Aaron Parecki <aaron@parecki.com>
2012-07-07 12:17:11 -0700

    New iPhone Tabbed project

---
+++ Geonotes/Geonotes/LQAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  LQAppDelegate.h
+//  Geonotes
+//
+//  Created by Aaron Parecki on 7/7/12.
+//  Copyright (c) 2012 Geoloqi, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LQAppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Geonotes/Geonotes/LQFirstViewController.m
@@ -0,0 +1,44 @@
+//
+//  LQFirstViewController.m
+//  Geonotes
+//
+//  Created by Aaron Parecki on 7/7/12.
+//  Copyright (c) 2012 Geoloqi, Inc. All rights reserved.
+//
+
+#import "LQFirstViewController.h"
+
+@interface LQFirstViewController ()
+
+@end
+
+@implementation LQFirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"First", @"First");
+        self.tabBarItem.image = [UIImage imageNamed:@"first"];
+    }
+    return self;
+}
+							
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
