8da081a9fbf796577cb5edba53b5c522bc20326d
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2012-07-18 17:07:21 -0700

    start on handling settings view row selects

--- Geonotes/Geonotes/LQSettingsViewController.h
+++ Geonotes/Geonotes/LQSettingsViewController.h
@@ -8,42 +8,14 @@
 
 #import <UIKit/UIKit.h>
 #import "Geoloqi.h"
-/*
-#import "LQSettingsActionSheetDelegate.h"
 
-static NSString *const LQAllowPublicGeonotesUserDefaultsKey = @"com.geoloqi.LQAllowPublicGeonotes";
-*/
-
-@interface LQSettingsViewController : UIViewController <UIAlertViewDelegate, UITableViewDataSource>
+@interface LQSettingsViewController : UIViewController <UIAlertViewDelegate, UITableViewDataSource, UITableViewDelegate>
 {
-    /*
-    UITextField *currentTextField;
-    BOOL keyboadIsShown;
-    LQSettingsActionSheetDelegate *actionDelegate;
-    */
     NSArray *sectionHeaders;
 }
 
-// @property (nonatomic) IBOutlet UIScrollView *scrollView;
-
 @property (strong) IBOutlet UISwitch *locationTracking;
-// @property (strong) IBOutlet UISwitch *allowPublicGeonotes;
-
-/*
-@property (strong) IBOutlet UIButton *publicGeonoteURL;
-@property (strong) IBOutlet UILabel *publicGeonoteURLLabel;
-
-@property (strong) IBOutlet UILabel *usernameLabel;
-@property (strong) IBOutlet UITextField *username;
-@property (strong) IBOutlet UIButton *saveUsername;
-@property (strong) IBOutlet UIActivityIndicatorView *savingIndicator;
-*/
 
 - (IBAction)locationTrackingWasSwitched:(UISwitch *)sender;
-/*
-- (IBAction)allowPublicGeonotesWasSwitched:(UISwitch *)sender;
-- (IBAction)saveUsernameWasTapped:(UIButton *)sender;
 
-- (IBAction)publicGeonoteURLWasTapped:(UIButton *)sender;
-*/
 @end
--- Geonotes/Geonotes/LQSettingsViewController.m
+++ Geonotes/Geonotes/LQSettingsViewController.m
@@ -10,23 +10,9 @@
 
 #define ANONUSER [LQSession savedSession].isAnonymous
 
-@interface LQSettingsViewController ()
-
-//- (LQSettingsActionSheetDelegate *)actionDelegate;
-
-@end
-
 @implementation LQSettingsViewController
 
-@synthesize // scrollView,
-            locationTracking/*,
-            allowPublicGeonotes,
-            usernameLabel,
-            username,
-            saveUsername,
-            savingIndicator*/;
-
-// @synthesize publicGeonoteURL, publicGeonoteURLLabel;
+@synthesize locationTracking;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -43,21 +29,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     NSLog(@"Settings View Loaded");
-/*    
-    scrollView.frame = CGRectMake(0, 88, 320, 416);
-    [scrollView setContentSize:CGSizeMake(320, 416)];
-    
-    self.locationTracking.on = ([[LQTracker sharedTracker] profile] != LQTrackerProfileOff);
-
-    if ([[NSUserDefaults standardUserDefaults] integerForKey:LQAllowPublicGeonotesUserDefaultsKey] == 1) {
-        self.allowPublicGeonotes.on = YES;
-        [self togglePublicGeonoteFields:YES];
-    } else {
-        self.allowPublicGeonotes.on = NO;
-    }
-    
-    [self populatePublicGeonoteFields];
-*/
 }
 
 - (void)viewDidUnload
@@ -72,7 +43,7 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-#pragma mark - Table View
+#pragma mark - Table View - Data Source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
@@ -154,7 +125,7 @@
     return [[self sectionHeaders] objectAtIndex:section];
 }
 
-#pragma mark - Cells
+#pragma mark - Table View - Cells
 
 - (UITableViewCell *)locationUpdateCell
 {
@@ -210,66 +181,45 @@
     return cell;
 }
 
-/*
-#pragma mark - Keyboard
+#pragma mark - Table View - Delegate
 
-- (void)viewWillAppear:(BOOL)animated
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardDidShow:)
-                                                 name:UIKeyboardDidShowNotification
-                                               object:self.view.window];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardDidHide:)
-                                                 name:UIKeyboardDidHideNotification
-                                               object:nil];
-    [super viewWillAppear:animated];
+    switch (indexPath.section) {
+        case 1:
+            switch (indexPath.row) {
+                case 0:
+                    if (ANONUSER)
+                        [self setupAccountCellWasTapped];
+                    else
+                        [self loginCellWasTapped];                    
+                    break;
+                case 1:
+                    [self loginCellWasTapped];                    
+                    break;
+            }
+            break;
+        case 2:
+            if (indexPath.row == 0)
+                [self privacyPolicyCellWasTapped];
+            break;
+    }
 }
 
-- (void)textFieldDidBeginEditing:(UITextField *)textField
+- (void)setupAccountCellWasTapped
 {
-    currentTextField = textField;
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField
-{
-    [textField resignFirstResponder];
-    return NO;
-}
-
-- (void)textFieldDidEndEditing:(UITextField *)textField
-{
-    currentTextField = nil;
-}
-
-- (void)keyboardDidShow:(NSNotification *)notification
-{
-    if (keyboadIsShown) return;
     
-    NSDictionary *info = [notification userInfo];
-    NSValue *aValue = [info objectForKey:UIKeyboardFrameEndUserInfoKey];
-    CGRect keyboardRect = [self.view convertRect:[aValue CGRectValue] fromView:nil];
-    CGRect viewFrame = [scrollView frame];
-    // not sure where these extra 5 pixels are coming from...
-    viewFrame.size.height -= (keyboardRect.size.height - 5);
-    scrollView.frame = viewFrame;
-    CGRect textFieldFrame = [currentTextField frame];
-    [scrollView scrollRectToVisible:textFieldFrame animated:YES];
-    keyboadIsShown = YES;
 }
 
-- (void)keyboardDidHide:(NSNotification *)notification
+- (void)loginCellWasTapped
 {
-    NSDictionary *info = [notification userInfo];
-    NSValue *aValue = [info objectForKey:UIKeyboardFrameEndUserInfoKey];
-    CGRect keyboardRect = [self.view convertRect:[aValue CGRectValue] fromView:nil];
-    CGRect viewFrame = [scrollView frame];
-    // not sure where these extra 5 pixels are coming from...
-    viewFrame.size.height += (keyboardRect.size.height - 5);
-    scrollView.frame = viewFrame;
-    keyboadIsShown = NO;
+    
 }
-*/
+
+- (void)privacyPolicyCellWasTapped
+{
+    
+}
 
 #pragma mark - IBActions
 
@@ -277,109 +227,5 @@
 {
     [[LQTracker sharedTracker] setProfile:(sender.on ? LQTrackerProfileAdaptive : LQTrackerProfileOff)];
 }
-
-/*
-- (IBAction)allowPublicGeonotesWasSwitched:(UISwitch *)sender
-{
-    int on = sender.on ? 1 : 0;
-    LQSession *session = [LQSession savedSession];
-    [self togglePublicGeonoteFields:sender.on];
-    NSDictionary *params = [[NSDictionary alloc] initWithObjectsAndKeys:[[NSString alloc] initWithFormat:@"%d", on], @"public_geonotes", nil];
-    [session runAPIRequest:[session requestWithMethod:@"POST" path:@"/account/privacy" payload:params]
-                completion:^(NSHTTPURLResponse *response, NSDictionary *responseDictionary, NSError *error) {
-                    // TODO handle response and errors if any
-                    [[NSUserDefaults standardUserDefaults] setInteger:on
-                                                              forKey:LQAllowPublicGeonotesUserDefaultsKey];
-                }
-     ];
-}
-
-- (IBAction)saveUsernameWasTapped:(UIButton *)sender
-{
-    if (currentTextField)
-        [currentTextField resignFirstResponder];
-    
-    NSDictionary *params = [[NSDictionary alloc] initWithObjectsAndKeys:username.text, @"desired_username", nil];
-    LQSession *session = [LQSession savedSession];
-    [self.savingIndicator startAnimating];
-    [session runAPIRequest:[session requestWithMethod:@"POST" path:@"/account/set_username" payload:params]
-                completion:^(NSHTTPURLResponse *response, NSDictionary *responseDictionary, NSError *error) {
-                    [self.savingIndicator stopAnimating];
-                    if (error) {
-                        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
-                                                                        message:[[error userInfo] objectForKey:@"NSLocalizedDescription"]
-                                                                       delegate:self
-                                                              cancelButtonTitle:@"OK"
-                                                              otherButtonTitles:nil];
-                        [alert show];
-                    }
-                    NSString *res = (NSString *)[responseDictionary objectForKey:@"response"];
-                    if (res && [res isEqualToString:@"ok"]) {
-                        [self populatePublicGeonoteFields:username.text];
-                        
-                        // hack to get new session data with the new username
-                        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"com.geoloqi.LQUserID"];
-                        
-                        // clear this so it gets rebuilt
-                        actionDelegate = nil;
-                        
-                        [LQSession sessionWithAccessToken:session.accessToken];
-                    }
-                }
-     ];
-}
-
-#pragma mark -
-
-- (IBAction)publicGeonoteURLWasTapped:(UIButton *)sender
-{
-    UIActionSheet *action = [[UIActionSheet alloc] initWithTitle:@""
-                                                        delegate:[self actionDelegate]
-                                               cancelButtonTitle:@"OK"
-                                          destructiveButtonTitle:nil
-                                               otherButtonTitles:@"Email", @"Text", @"Copy", nil];
-    [action showFromTabBar:self.tabBarController.tabBar];
-}
-
-- (LQSettingsActionSheetDelegate *)actionDelegate
-{
-    if (actionDelegate) return actionDelegate;
-    return actionDelegate = [[LQSettingsActionSheetDelegate alloc] initWithUsername:[[LQSession savedSession] username]
-                                                                  andViewController:self];
-}
-
-#pragma mark -
-
-- (void)togglePublicGeonoteFields:(BOOL)show
-{
-    self.publicGeonoteURL.hidden = !show;
-    self.publicGeonoteURLLabel.hidden = !show;
-
-    self.usernameLabel.hidden = !show;
-    self.username.hidden = !show;
-    self.saveUsername.hidden = !show;
-    self.saveUsername.enabled = show;
-}
-
-- (void)populatePublicGeonoteFields
-{
-    [self populatePublicGeonoteFields:[[LQSession savedSession] username]];
-}
-
-- (void)populatePublicGeonoteFields:(NSString *)_username
-{
-    [self.publicGeonoteURL setTitle:[NSString stringWithFormat:@"http://loqi.me/%@", _username]
-                           forState:UIControlStateNormal];
-    self.username.text = _username;
-}
-
-#pragma mark - UIAlertViewDelegate
-
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
-{
-    [self populatePublicGeonoteFields:[[LQSession savedSession] username]];
-    [self.username becomeFirstResponder];
-}
-*/
 
 @end
