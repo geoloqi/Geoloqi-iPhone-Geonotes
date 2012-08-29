10e63156ec2367a9e1626877f61707b828212734
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2012-08-29 15:59:13 -0700

    sdk bump

--- Geonotes/GeoloqiSDK/LQSDKVersion.h
+++ Geonotes/GeoloqiSDK/LQSDKVersion.h
@@ -8,7 +8,7 @@
 //static NSString *const LQSDKNameString = @"com.geoloqi.titanium.iphone";
 static NSString *const LQSDKNameString = @"com.geoloqi.iphone";
 
-static NSString *const LQSDKVersionString = @"12.237";
+static NSString *const LQSDKVersionString = @"12.242";
 
 // This build string is automatically updated when the sdk is built! Do not modify.
 static NSString *const LQSDKVersionGitVersion = @"a1e19bf9c0781434b79c6730b45f93dfbf3bbe42";
