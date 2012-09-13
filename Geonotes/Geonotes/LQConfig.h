3411f53db2791ef251eda887c6ad9c793ff9c76d
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2012-09-13 14:37:34 -0700

    retina4 splash + remove creds from LQConfig (again)

--- Geonotes/Geonotes/LQConfig.h
+++ Geonotes/Geonotes/LQConfig.h
@@ -3,6 +3,6 @@
 // enter your API Key and Secret here.
 // https://developers.geoloqi.com/
 
-static NSString *const LQ_APIKey = @"44a1f66f7bf41d9063f1fe5c558f40dc";
-static NSString *const LQ_APISecret = @"a7dc66e533847611c8f109db57ce50d6";
+static NSString *const LQ_APIKey = @"";
+static NSString *const LQ_APISecret = @"";
 
